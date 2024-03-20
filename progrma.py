3222b12c46dba51b51d3bb9f85b498439f23aaf2
mokshith-moksh <121559082+mokshith-moksh@users.noreply.github.com>
2024-03-20 22:42:23 +0530

    Update progrma.py

--- progrma.py
+++ progrma.py
@@ -103,7 +103,7 @@ plt.show()
 import matplotlib.pyplot as plt
 x1 = [89, 43, 36, 36, 95, 10, 66, 34, 38, 20]
 y1 = [21, 46, 3, 35, 67, 95, 53, 72, 58, 10]
-plt.scatter(x1, y1, c="pink", linewidths=2, marker="s", edgecolor="green", s=50)
+plt.scatter(x1, y1)
 plt.xlabel("X-axis")
 plt.ylabel("Y-axis")
 plt.show()
