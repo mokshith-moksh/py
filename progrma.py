e3218b3f104b069721ef21d0166c9a5fb4537088
mokshith-moksh <121559082+mokshith-moksh@users.noreply.github.com>
2024-03-21 10:03:54 +0530

    Update progrma.py

--- progrma.py
+++ progrma.py
@@ -157,8 +157,6 @@ def sinplot(n=10):
     x = np.linspace(0, 14, 100)
     for i in range(1, n + 1):
         plt.plot(x, np.sin(x + i * 5) * (n + 2 - i))
-sns.set_theme()
-sns.set_context("notebook", font_scale=1.5, rc={"lines.linewidth": 2.5})
 sinplot()
 plt.title('Seaborn plots with Aesthetic functions')
 plt.show()
